679f2e382facb37892f16fcee62541030c87806d
Nepfortress <nepfortress2006@gmail.com>
2025-10-28 20:52:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 # Magic Type
 
 import pygame
-from display import *
 from background import *
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((screen_length, screen_width))
+screen = pygame.display.set_mode((1920, 1080)) # Game resolution 1920 pixel times 1080 pixels
 clock = pygame.time.Clock()
 title = pygame.display.set_caption("Magic Type")
 running = True
 dt = 0
 
 while running:
-    # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # fill the screen with Monument Valley
+
+    # fill background with a fhd picture 
     screen.fill((0, 0, 0))
     screen.blit(bg, (0, 0))
 
