466cf3a333a36553819f4149245f7ab142af46e6
Nepfortress <nepfortress2006@gmail.com>
2025-10-29 22:54:39 +0700

    Update background.py

--- background.py
+++ background.py
@@ -1,7 +1,6 @@
 import pygame
 
-# https://www.peppercarrot.com/0_sources/0ther/wallpapers/hi-res/2017-04-10_Only-Komona_by-David-Revoy.jpg
-bg = pygame.image.load("2017-04-10_Only-Komona_by-David-Revoy.jpg")
+bg = pygame.image.load("magicimg/softmoutain.png")
 def backgroundImg(bg):
     size = pygame.transform.scale(bg, (1920, 1080))
     screen.blit(size, (0, 0))
